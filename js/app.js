3e7fbe2534fbc69aaf7f3dc5611b325e2b3bcab4
Ryan Zajac <ryan4life12@gmail.com>
2017-01-19 11:55:05 -0600

    a

--- js/app.js
+++ js/app.js
@@ -1,35 +1,129 @@
-  $('#i').keyup(function () {
-            if (event.keyCode == 13) {
-                $('#submit').click();
-            } else {
-                return false
-            }
-        });
- $('#submit').click(function () {
-            var c = $('.search-form').serialize();
-            var d = 'http://www.omdbapi.com/?' + c;
-            var e = $('#search-by-title-request');
-            e.find('a').attr('href', d).html(d);
-            e.show('slow');
-            var f = $('#search-by-title-progress');
-            f.show('slow');
-            var g = $('#search-by-title-response');
-            $.ajax({
-                type: 'GET',
-                dataType: 'text',
-                url: '/?' + c,
-                statusCode: {
-                    403: function () {
-                        g.find('pre').html('HTTP 403 Forbidden!')
-                    }
-                },
-                success: function (a) {
-                    g.find('pre').html(a.replace(/</g, '&lt;').replace(/>/g, '&gt;'))
-                },
-                complete: function () {
-                    f.hide();
-                    g.show('slow')
-                }
-            })
-        });
-       
+$('.search-form').submit(function (evt) {
+    // body...
+    evt.preventDefault();
+    var omdbApi = 'http://www.omdbapi.com/?';
+    var movieSearchTerm = $('#search').val();
+    var searchData = {
+        t:movieSearchTerm,
+        format: json
+    }
+    function displayMovies(data) {
+        // body...
+        var movieHTML = '<li>';
+        $.each(data.items,function(i,movie) {
+        movieHTML += '<li class="desc">';
+        movieHTML += 
+        movieHTML += '<a href="' + movie.link + '" class="image">';
+        movieHTML += '<img src="' + movie.media.Poster + '"></a></li>';
+      }); // end each
+      movieHTML += '</ul>';
+      $('#movies').html(movieHTML);
+    }
+    $.getJSON(omdbApi, searchData, displayMovies);
+});//end submit
+
+
+// !function ($) {
+//     $(function () {
+//         var j = false;
+//         $('#navbar-main li a, .navbar-header a').click(function () {
+//             var a = $(this);
+//             if (a.attr('href') == '#top') {
+//                 $('html,body').animate({ 'scrollTop': 0 }, 1000);
+//             }
+//             else {
+//                 var b = $(a.attr('href'));
+//                 $('html,body').animate({ 'scrollTop': b.offset().top - 50 }, 1000);
+//             }
+//             return false
+//         });
+//         $('#t').keyup(function () {
+//             if (event.keyCode == 13) {
+//                 $('#search-by-title-button').click();
+//             } else {
+//                 return false
+//             }
+//         });
+//         $('#i').keyup(function () {
+//             if (event.keyCode == 13) {
+//                 $('#search-by-id-button').click();
+//             } else {
+//                 return false
+//             }
+//         });
+//         $('#search-by-id-button').click(function () {
+//             var c = $('#search-by-id-form').serialize();
+//             var d = 'http://www.omdbapi.com/?' + c;
+//             var e = $('#search-by-id-request');
+//             e.find('a').attr('href', d).html(d);
+//             e.show('slow');
+//             var f = $('#search-by-id-progress');
+//             f.show('slow');
+//             var g = $('#search-by-id-response');
+//             $.ajax({
+//                 type: 'GET',
+//                 dataType: 'text',
+//                 url: '/?' + c,
+//                 statusCode: {
+//                     403: function () {
+//                         g.find('pre').html('HTTP 403 Forbidden!')
+//                     }
+//                 },
+//                 success: function (a) {
+//                     g.find('pre').html(a.replace(/</g, '&lt;').replace(/>/g, '&gt;'))
+//                 },
+//                 complete: function () {
+//                     f.hide();
+//                     g.show('slow')
+//                 }
+//             })
+//         });
+//         $('#search-by-id-reset').click(function () {
+//             var a = $('#search-by-id-request');
+//             a.hide('slow');
+//             a.find('a').attr('href', 'javascript:;').html('');
+//             var b = $('#search-by-id-progress');
+//             b.hide('slow');
+//             var c = $('#search-by-id-response');
+//             c.hide('slow');
+//             c.find('pre').html('');
+//         });
+//         $('#search-by-title-button').click(function () {
+//             var c = $('#search-by-title-form').serialize();
+//             var d = 'http://www.omdbapi.com/?' + c;
+//             var e = $('#search-by-title-request');
+//             e.find('a').attr('href', d).html(d);
+//             e.show('slow');
+//             var f = $('#search-by-title-progress');
+//             f.show('slow');
+//             var g = $('#search-by-title-response');
+//             $.ajax({
+//                 type: 'GET',
+//                 dataType: 'text',
+//                 url: '/?' + c,
+//                 statusCode: {
+//                     403: function () {
+//                         g.find('pre').html('HTTP 403 Forbidden!')
+//                     }
+//                 },
+//                 success: function (a) {
+//                     g.find('pre').html(a.replace(/</g, '&lt;').replace(/>/g, '&gt;'))
+//                 },
+//                 complete: function () {
+//                     f.hide();
+//                     g.show('slow')
+//                 }
+//             })
+//         });
+//         $('#search-by-title-reset').click(function () {
+//             var a = $('#search-by-title-request');
+//             a.hide('slow');
+//             a.find('a').attr('href', 'javascript:;').html('');
+//             var b = $('#search-by-title-progress');
+//             b.hide('slow');
+//             var c = $('#search-by-title-response');
+//             c.hide('slow');
+//             c.find('pre').html('');
+//         });
+//     })
+// } (window.jQuery);
